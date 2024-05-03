2e73f850fc4bd9bc1580080bce1f7b1d9b502ac3
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-03 20:23:48 +0900

    feat: buckler gateway

---
+++ app/services/buckler_gateway.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class BucklerGateway
+  def initialize
+    credentials = BucklerCredential.fetch
+    @api = Buckler.build_api(credentials)
+  end
+
+  def method_missing(name, *, &)
+    @api.public_send(name, *, &)
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    @api.respond_to?(name, include_private)
+  end
+end
