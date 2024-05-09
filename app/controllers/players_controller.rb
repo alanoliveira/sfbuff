8bd7b3795ed033527dfc0bd4e9679721476134df
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-09 16:12:26 +0900

    feat: battle filtering

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,6 +8,10 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:sid])
     battles = PlayerBattles.fetch(@player.sid)
+                           .using_character(params[:player_character])
+                           .using_control_type(params[:player_control_type])
+                           .vs_character(params[:vs_character])
+                           .vs_control_type(params[:vs_control_type])
     @rivals = Rivals.fetch(battles)
     @battles = battles.page(params[:page])
 
