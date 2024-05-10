4508290934379ce231fd9905439703de7f38f108
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-10 19:41:16 +0900

    fix: set memcached auth params

--- config/initializers/cacheable_job.rb
+++ config/initializers/cacheable_job.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  CacheableJob.cache_provider = if Rails.env.production?
-                                  ActiveSupport::Cache::MemCacheStore.new(ENV.fetch('MEMCACHED_HOST'))
-                                else
-                                  ActiveSupport::Cache::MemoryStore.new
-                                end
+  CacheableJob.cache_provider =
+    if Rails.env.production?
+      ActiveSupport::Cache::MemCacheStore.new(
+        ENV.fetch('MEMCACHED_HOST'),
+        {
+          username: ENV.fetch('MEMCACHED_USERNAME', nil),
+          password: ENV.fetch('MEMCACHED_PASSWORD', nil)
+        }
+      )
+    else
+      ActiveSupport::Cache::MemoryStore.new
+    end
 end
