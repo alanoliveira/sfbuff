2fa89521fd6e5ac5d62095cfb125a941fd39d9d5
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-12 13:40:26 +0900

    chore: configure sync job concurrency

--- app/jobs/player_sync_job.rb
+++ app/jobs/player_sync_job.rb
@@ -2,11 +2,14 @@
 
 class PlayerSyncJob < ApplicationJob
   include CacheableJob
+  limits_concurrency to: 1, key: ->(player_sid) { player_sid }
 
   queue_as :default
 
   def perform(player_sid)
-    PlayerSynchronizer.new(player_sid, BucklerGateway.new).synchronize
+    unless Player.find_by(sid: player_sid).try(&:synchronized?)
+      PlayerSynchronizer.new(player_sid, BucklerGateway.new).synchronize
+    end
     cache(:success)
   rescue StandardError => e
     cache(:error, { class: e.class.name, message: e.message })
