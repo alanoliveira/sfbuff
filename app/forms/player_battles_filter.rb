002c753ec82af64d67e0d416a0e2daf68453c01a
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-12 18:20:07 +0900

    refactor: refactor battle filter

--- app/forms/player_battles_filter.rb
+++ app/forms/player_battles_filter.rb
@@ -4,22 +4,22 @@ class PlayerBattlesFilter
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  attribute :using_character, :integer
-  attribute :using_control_type, :integer
-  attribute :vs_character, :integer
-  attribute :vs_control_type, :integer
+  attribute :player_character, :integer
+  attribute :player_control_type, :integer
+  attribute :opponent_character, :integer
+  attribute :opponent_control_type, :integer
   attribute :battle_type, :integer
   attribute :played_at_from, :date, default: 7.days.ago
   attribute :played_at_to, :date
 
-  FILTER_ATTR = %i[using_character using_control_type
-                   vs_character vs_control_type battle_type
-                   played_at].freeze
-
-  def inject(player_battles)
-    FILTER_ATTR.inject(player_battles) do |battles, filter|
-      battles.public_send(filter, send(filter))
-    end
+  def inject(battles_pov)
+    battles_pov.where(
+      {
+        player: player_cond,
+        opponent: opponent_cond,
+        **battle_cond
+      }.compact_blank
+    )
   end
 
   def played_at
@@ -28,5 +28,28 @@ class PlayerBattlesFilter
     return if [from, to].all?(:nil?)
 
     from..to
+  end
+
+  private
+
+  def player_cond
+    {
+      character: player_character,
+      control_type: player_control_type
+    }.compact_blank
+  end
+
+  def opponent_cond
+    {
+      character: opponent_character,
+      control_type: opponent_control_type
+    }.compact_blank
+  end
+
+  def battle_cond
+    {
+      played_at:,
+      battle_type:
+    }.compact_blank
   end
 end
