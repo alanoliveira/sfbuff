526cfea1830b0f2cf176fa462c470e42496fc203
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-14 18:08:53 +0900

    fix: skip credential initialization in local envs

--- config/initializers/buckler_credential.rb
+++ config/initializers/buckler_credential.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
+  next unless Rails.env.production?
+
   BucklerCredential.store(Buckler::Credentials.new) if BucklerCredential.read.nil?
 end
