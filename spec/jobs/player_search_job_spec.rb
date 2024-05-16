79cb7f1abd493363941b4dc108f12a01f9cd6c88
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-16 16:13:11 +0900

    chore: CI

--- spec/jobs/player_search_job_spec.rb
+++ spec/jobs/player_search_job_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe PlayerSearchJob, type: :job do
+RSpec.describe PlayerSearchJob do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/jobs/player_sync_job_spec.rb
+++ spec/jobs/player_sync_job_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe PlayerSyncJob, type: :job do
+RSpec.describe PlayerSyncJob do
   pending "add some examples to (or delete) #{__FILE__}"
 end
