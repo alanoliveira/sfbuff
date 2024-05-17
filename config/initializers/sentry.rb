67d6fc3e1313eec935e36f9db618d92527e5b47b
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-17 16:57:46 +0900

    chore: adjust sentry profiles/traces rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,5 +2,8 @@ Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enable_tracing = true
-  config.enabled_environments = ['production']
+  config.enabled_environments = [:production]
+
+  config.traces_sample_rate = 0.1
+  config.profiles_sample_rate = 0.1
 end
