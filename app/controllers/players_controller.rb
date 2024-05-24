1101b88a5ffefab626baf643aac576e1f3018e17
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-24 11:29:19 +0900

    adjust routes and layout

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,22 +1,33 @@
 # frozen_string_literal: true
 
 class PlayersController < ApplicationController
+  before_action :set_player, only: %i[show battles]
+
   # GET /players
   def index; end
 
   # GET /players/:sid
   def show
-    @player = Player.find(params[:sid])
+    redirect_to battles_player_url(@player)
+  end
+
+  def battles
     @filter = PlayerBattlesFilter.new(filter_params)
     @battles = @player.battles.includes(:p1, :p2).where(@filter.as_where).reorder(played_at: :desc)
     @page_battles = @battles.page(params[:page])
 
-    render partial: 'battles' if turbo_frame_request_id == 'battle-list'
-  rescue ActiveRecord::RecordNotFound
-    # do nothing
+    return render partial: 'battles' if turbo_frame_request_id == 'battle-list'
+
+    render 'battles'
   end
 
   private
+
+  def set_player
+    @player = Player.find_or_initialize_by(sid: params[:sid])
+
+    render html: nil, layout: true if @player.new_record?
+  end
 
   def filter_params
     params.permit(PlayerBattlesFilter.attribute_names)
--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -3,32 +3,100 @@
 require 'rails_helper'
 
 RSpec.describe '/players' do
-  describe 'GET /players/:sid' do
-    context 'when the player exists' do
-      let(:player) { create(:player) }
+  let(:player) { create(:player) }
 
-      it 'renders a successful response' do
-        get players_url(player)
-        expect(response).to be_successful
-      end
+  RSpec::Matchers.define :has_player_sync_div do
+    match { |subject| values_match?(match_xpath("//div[@id='player-sync']"), subject) }
+    failure_message { 'body do not contains the player sync div' }
+    failure_message_when_negated { 'body should not contains the player sync div' }
+  end
 
-      it 'renders the player-sync div' do
-        get player_url(player)
-        expect(response.body).to match_xpath("//div[@id='player-sync']")
+  shared_examples 'a layout/players page' do
+    context 'when player exists and is synchronized' do
+      let(:player) { create(:player, :synchronized) }
+
+      it do
+        expect(response.body).not_to has_player_sync_div if response.successful?
       end
     end
 
-    context 'when the player do not exists' do
+    context 'when player exists but not synchronized' do
+      let(:player) { create(:player, :desynchronized) }
+
+      it do
+        expect(response.body).to has_player_sync_div if response.successful?
+      end
+    end
+
+    context 'when player do not exists' do
       let(:player) { build(:player) }
 
-      it 'renders the player-sync div' do
-        get player_url(player)
-        expect(response.body).to match_xpath("//div[@id='player-sync']")
+      it do
+        expect(response.body).to has_player_sync_div if response.successful?
+      end
+    end
+  end
+
+  describe 'GET /players/:sid' do
+    it_behaves_like 'a layout/players page' do
+      before { get player_url(player) }
+    end
+
+    it do
+      get player_url(player)
+      expect(response).to redirect_to(battles_player_url(player))
+    end
+  end
+
+  describe 'GET /players/:sid/battles' do
+    it_behaves_like 'a layout/players page' do
+      before { get battles_player_url(player) }
+    end
+
+    it do
+      get battles_player_url(player)
+      expect(response).to be_successful
+    end
+
+    it do
+      get battles_player_url(player)
+      expect(response.body).to match_xpath('//h3[text()="Rivals"]')
+    end
+
+    it do
+      get battles_player_url(player)
+      expect(response.body).to match_xpath('//h3[text()="Matches"]')
+    end
+
+    context 'when player have no matches' do
+      it 'render an alert with the number of battles found' do
+        get battles_player_url(player)
+        expect(response.body).to match_xpath('//div[@role="alert" and text()="No matches found"]')
+      end
+    end
+
+    context 'when player have matches' do
+      before do
+        create(:battle, winner_side: 1, battle_type: 1, played_at: 1.minute.ago,
+                        p1: { player_sid: player.sid, rounds: [0, 1, 2] },
+                        p2: { rounds: [3, 4, 5] })
+
+        create(:battle, winner_side: 2, battle_type: 2, played_at: 1.minute.ago,
+                        p1: { player_sid: player.sid, rounds: [6, 7] },
+                        p2: { rounds: [8, 8] })
+
+        create(:battle, winner_side: nil, battle_type: 3, played_at: 1.minute.ago,
+                        p1: {},
+                        p2: { player_sid: player.sid })
+
+        create(:battle, battle_type: 4, played_at: 1.minute.ago,
+                        p1: {},
+                        p2: { player_sid: player.sid })
       end
 
-      it 'renders a successful response' do
-        get player_url(player)
-        expect(response).to be_successful
+      it 'render an alert with the number of battles found' do
+        get battles_player_url(player)
+        expect(response.body).to match_xpath('//div[@role="alert" and text()="4 matches found"]')
       end
     end
   end
