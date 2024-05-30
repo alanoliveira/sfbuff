149004524355ae570ba84402dd7e73335cc7c826
Alan Oliveira <alan.oliveira89@gmail.com>
2024-05-30 17:32:38 +0900

    fix sentry configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,8 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
+  config.background_worker_threads = 1
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.enable_tracing = true
-  config.enabled_environments = [:production]
-
-  config.traces_sample_rate = 0.1
-  config.profiles_sample_rate = 0.1
+  config.enable_tracing = false
+  config.enabled_environments = ['production']
+  config.excluded_exceptions += ['Buckler::Client::ServerUnderMaintenance']
 end
