10ec988371cff6e48479a8880224e8aa855d82ea
Alan Oliveira <alan.oliveira89@gmail.com>
2024-06-21 22:08:11 +0900

    workaround for error messages translation

--- app/jobs/concerns/result_broadcastable.rb
+++ app/jobs/concerns/result_broadcastable.rb
@@ -5,7 +5,9 @@ module ResultBroadcastable
 
   included do
     rescue_from(StandardError) do |error|
-      broadcast_result('error', { kind: resolve_error_kind(error) })
+      I18n.with_locale(locale) do
+        broadcast_result('error', { kind: resolve_error_kind(error) })
+      end
       raise error
     end
   end
