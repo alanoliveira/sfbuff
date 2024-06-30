412c1079ba3ce80e087e8ec58c3a8a4e9631dba2
Alan Oliveira <alan.oliveira89@gmail.com>
2024-06-30 22:38:47 +0900

    refactory player sync threshold

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 class Player < ApplicationRecord
+  def self.sync_threshold
+    Rails.configuration.sfbuff['data_sync_threshold'].seconds.ago
+  end
+
   def synchronized?
-    synchronized_at.present? && synchronized_at > sync_threshold
+    synchronized_at.present? && synchronized_at > Player.sync_threshold
   end
 
   def battles
     Battle.pov.where(player: { player_sid: sid })
-  end
-
-  private
-
-  def sync_threshold
-    Rails.configuration.sfbuff['data_sync_threshold'].seconds.ago
   end
 end
