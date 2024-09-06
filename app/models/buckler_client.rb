ade43633348aa916c49a74a1d40bb252bed5ac98
Alan Oliveira <alan.oliveira89@gmail.com>
2024-09-06 16:53:36 +0900

    rescue from errors in buckler_login

--- app/models/buckler_client.rb
+++ app/models/buckler_client.rb
@@ -1,5 +1,14 @@
 class BucklerClient < ApplicationRecord
+  include ActiveSupport::Rescuable
+
   def api
-    @api ||= Buckler::Api::Client.new(cookies:, build_id:)
+    @api ||= Buckler::Api::Client.new(cookies:, build_id:, connection:)
+  end
+
+  private
+
+  def connection
+    rescue_handler = ->(e) { rescue_with_handler(e) || raise }
+    Buckler::Api::Connection.build(rescue_handler:)
   end
 end
