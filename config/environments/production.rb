f4927e29ae8714724b84ec59a2a3979a2cfb634b
Alan Oliveira <alan.oliveira89@gmail.com>
2024-09-07 18:08:36 +0900

    prepare production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "sfbuff_production"
+  # Use a async to keep low budget
+  config.active_job.queue_adapter = :async
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
