cf675647f5a11676932a9f6f3457ba5340ae2a20
Alan Oliveira <alan.oliveira89@gmail.com>
2024-09-09 16:30:26 +0900

    identify cable connections using session_id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,5 +3,12 @@ module ApplicationCable
     include Internationalizable
 
     around_command :switch_locale, :switch_timezone
+
+    identified_by :session_id
+
+    def connect
+      reject_unauthorized_connection if request.session.id.blank?
+      self.session_id = request.session.id
+    end
   end
 end
