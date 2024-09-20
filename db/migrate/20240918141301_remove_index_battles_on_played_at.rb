e5c349b57eb0312622c77bed2a1ae9b86fc797a8
Alan Oliveira <alan.oliveira89@gmail.com>
2024-09-20 21:10:47 +0900

    Revert "remove index battles (played_at) (#21)"
    This reverts commit d5b939bedaf5adfb6398c6421218706c0b88c0c6.

--- db/migrate/20240918141301_remove_index_battles_on_played_at.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIndexBattlesOnPlayedAt < ActiveRecord::Migration[7.2]
-  def change
-    remove_index :battles, :played_at
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_18_141301) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_112222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_18_141301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["battle_type"], name: "index_battles_on_battle_type"
+    t.index ["played_at"], name: "index_battles_on_played_at"
     t.index ["replay_id"], name: "index_battles_on_replay_id", unique: true
   end
 
