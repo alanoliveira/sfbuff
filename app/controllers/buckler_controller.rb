391055ce51639d4f508eee3182841e41cc4636be
Alan Oliveira <alan.oliveira89@gmail.com>
2024-09-30 16:06:52 +0900

    refactor search form

--- app/controllers/buckler_controller.rb
+++ app/controllers/buckler_controller.rb
@@ -2,5 +2,6 @@ class BucklerController < ApplicationController
   layout "with_header_footer"
 
   def player_search
+    @term = params[:q]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,17 +18,6 @@ module ApplicationHelper
                 title: l(time, format: :short)
   end
 
-  def search_form(**, &block)
-    form_with url: buckler_player_search_path, method: :get, role: "search", ** do |f|
-      content_tag :div, class: "input-group" do
-        concat (f.text_field :q, value: params[:q], placeholder: t("helpers.search_form.placeholder"),
-          class: "form-control", type: "search", required: true, minlength: 4)
-
-        yield f if block_given?
-      end
-    end
-  end
-
   def spinner(**opts)
     content_tag :div, class: [ "spinner-border", opts.delete(:class) ], role: "status" do
       content_tag :span, "#{t("aria.texts.loading")}...", class: "visually-hidden"
