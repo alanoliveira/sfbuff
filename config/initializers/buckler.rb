1f9afba44db7dedefa7fb13325788fc27b0601bb
Alan Oliveira <alan.oliveira89@gmail.com>
2024-11-11 21:16:22 +0900

    remove `fetch` from buckler initializer

--- config/initializers/buckler.rb
+++ config/initializers/buckler.rb
@@ -1,9 +1,9 @@
 Rails.application.config.to_prepare do
   Buckler.configure do |conf|
-    conf.base_url = ENV.fetch("BUCKLER_BASE_URL") { "https://www.streetfighter.com" }
-    conf.user_agent = ENV.fetch("BUCKLER_USER_AGENT")
-    conf.email = ENV.fetch("BUCKLER_EMAIL")
-    conf.password = ENV.fetch("BUCKLER_PASSWORD")
-    conf.logger = Rails.logger.tagged("BucklerApi")
+    conf.base_url = ENV.fetch("BUCKLER_BASE_URL", "https://www.streetfighter.com")
+    conf.user_agent = ENV["BUCKLER_USER_AGENT"]
+    conf.email = ENV["BUCKLER_EMAIL"]
+    conf.password = ENV["BUCKLER_PASSWORD"]
+    conf.logger = Rails.logger.tagged("BucklerApi", "")
   end
 end
