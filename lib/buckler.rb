1aa07ff33ebf393be51003a3ade6c219e5be903d
Alan Oliveira <alan.oliveira89@gmail.com>
2024-11-11 16:23:36 +0900

    add a default client do Buckler module

--- lib/buckler.rb
+++ lib/buckler.rb
@@ -10,5 +10,19 @@ module Buckler
     def configure
       yield(configuration)
     end
+
+    def default_client
+      @default_client ||= Client.new
+    end
+
+    private
+
+    def respond_to_missing?(...)
+      default_client.respond_to?(...) || super
+    end
+
+    def method_missing(method_name, ...)
+      default_client.send(method_name, ...)
+    end
   end
 end
