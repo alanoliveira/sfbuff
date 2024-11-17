d23981112a399aa073065f4f7b318be9b90aefb0
Alan Oliveira <alan.oliveira89@gmail.com>
2024-11-17 15:34:34 +0900

    temporary workaround for parse played_at on ranked screen

--- app/controllers/players/rankeds_controller.rb
+++ app/controllers/players/rankeds_controller.rb
@@ -6,11 +6,23 @@ class Players::RankedsController < Players::BaseController
   def show
     @ranked_history = RankedHistory.new(
       short_id: params[:short_id], character: params[:character],
-      played_at: (params[:played_from]..params[:played_to])
+      played_at: (played_from..played_to)
     )
   end
 
   private
+
+  def played_from
+    Time.zone.parse(params[:played_from]).beginning_of_day
+  rescue ArgumentError
+    Time.zone.now.beginning_of_day
+  end
+
+  def played_to
+    Time.zone.parse(params[:played_to]).end_of_day
+  rescue ArgumentError
+    Time.zone.now.end_of_day
+  end
 
   def default_params
     {
