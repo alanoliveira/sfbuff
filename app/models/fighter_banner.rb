6bc2fc9bb5367ad1d551cc262a71016f143abbde
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-02 19:20:29 +0900

    add fighter_banner find methods to its model

--- app/models/fighter_banner.rb
+++ app/models/fighter_banner.rb
@@ -2,6 +2,26 @@ class FighterBanner
   include ActiveModel::API
   include ActiveModel::Attributes
 
+  class << self
+    def find(short_id)
+      ShortId.new(short_id) rescue return nil
+
+      search_map(short_id:).first
+    end
+
+    def search(query)
+      [ find(query) ].compact | search_map(fighter_id: query)
+    end
+
+    private
+
+    def search_map(**)
+      BucklerApi.fighterslist(**).map do |raw_data|
+        Parsers::FighterBannerParser.parse(raw_data:)
+      end
+    end
+  end
+
   attribute :short_id, :buckler_short_id
   attribute :name
   attribute :main_character, :buckler_character
