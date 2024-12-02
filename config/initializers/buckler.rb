5249bebabcb1955257c5a01996ed136872c8f6e4
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-02 20:04:48 +0900

    delete old buckler api implementation

--- config/initializers/buckler.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.to_prepare do
-  Buckler.configure do |conf|
-    conf.base_url = ENV.fetch("BUCKLER_BASE_URL", "https://www.streetfighter.com")
-    conf.user_agent = ENV["BUCKLER_USER_AGENT"]
-    conf.email = ENV["BUCKLER_EMAIL"]
-    conf.password = ENV["BUCKLER_PASSWORD"]
-    conf.logger = Rails.logger.tagged("BucklerApi", "")
-  end
-end
