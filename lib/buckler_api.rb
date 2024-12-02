79b6379f16f85f36630cd0356c28a9bf1c080584
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-02 15:10:40 +0900

    implement default BucklerApi connection

--- lib/buckler_api.rb
+++ lib/buckler_api.rb
@@ -4,10 +4,28 @@ module BucklerApi
 
   class << self
     attr_writer :logger
-    attr_accessor :username, :password
+    attr_accessor :email, :password
 
     def logger
       @logger ||= Logger.new(IO::NULL)
+    end
+
+    def connection
+      @connection ||= Connection.new(
+        adapter: FaradayAdapter,
+        build_id: StrategySelector.new(-> { ENV["BUCKLER_BUILD_ID"] }, BuildIdStrategies::Faraday),
+        auth_cookies: StrategySelector.new(-> { ENV["BUCKLER_AUTH_COOKIES"] }, AuthCookiesStrategies::Selenium.new(email:, password:)),
+      )
+    end
+
+    private
+
+    def respond_to_missing?(...)
+      connection.respond_to?(...)
+    end
+
+    def method_missing(method_name, ...)
+      connection.public_send(method_name, ...)
     end
   end
 
