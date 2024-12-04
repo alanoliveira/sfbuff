5b2670e0a3fca8006be57e0a7251ec5ffdca9785
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-04 18:56:03 +0900

    implement matchup

---
+++ app/models/matchup.rb
@@ -0,0 +1,60 @@
+class Matchup
+  include ActiveModel::API
+  include Enumerable
+
+  RESULT_SELECT = <<-SQL.squeeze(" ")
+    CASE
+    WHEN "battles"."winner_side" = "home"."side" THEN 'win'
+    WHEN "battles"."winner_side" = "away"."side" THEN 'lose'
+    ELSE 'draw'
+    END "result"
+  SQL
+
+  JOIN_STATEMENT = <<-SQL.squeeze(" ")
+    INNER JOIN "home" ON "home".battle_id = "battles"."id"
+    INNER JOIN "away" ON "away".battle_id = "battles"."id"
+                     AND "away"."side" = CASE WHEN "home"."side" = 1 THEN 2 ELSE 1 END
+  SQL
+
+  attr_accessor :battle, :home, :away
+  delegate :count, :cache_key, :cache_version, to: :relation
+
+  def initialize(battle: nil, home: nil, away: nil)
+    @battle = (battle || Battle).all
+    @home = (home || Challenger).all
+    @away = (away || Challenger).all
+  end
+
+  %i[limit offset order].each do |method_name|
+    class_eval(<<-RUBY, __FILE__, __LINE__+1)
+      def #{method_name}(...)
+        #{name}.new(battle: battle.#{method_name}(...), home:, away:)
+      end
+
+      def #{method_name}!(...)
+        battle.#{method_name}!(...)
+        self
+      end
+    RUBY
+  end
+
+  def each
+    battles = relation.includes(:challengers).index_by(&:id)
+    relation
+      .pluck("id", "home.id", "away.id", Arel.sql(RESULT_SELECT))
+      .each do |battle_id, home_id, away_id, result|
+        yield({
+          battle: battles[battle_id],
+          home: battles[battle_id].challengers.find { |c| c.id == home_id },
+          away: battles[battle_id].challengers.find { |c| c.id == away_id },
+          result:
+        })
+      end
+  end
+
+  private
+
+  def relation
+    battle.joins(JOIN_STATEMENT).with(home:, away:)
+  end
+end
