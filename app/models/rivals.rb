566eaa29af555dd7873da02726ab1e940377bf8c
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-06 13:56:21 +0900

    implement rivals model

---
+++ app/models/rivals.rb
@@ -0,0 +1,47 @@
+class Rivals
+  include ActiveModel::API
+
+  attr_reader :matchup, :limit
+
+  def initialize(matchup, limit: nil)
+    @matchup = matchup
+    @limit = limit
+  end
+
+  def favorites(&)
+    enumerate("total" => :desc, &)
+  end
+
+  def victims(&)
+    enumerate("diff" => :desc, "win" => :desc, "lose" => :asc, &)
+  end
+
+  def tormentors(&)
+    enumerate("diff" => :asc, "lose" => :desc, "win" => :asc, &)
+  end
+
+  private
+
+  def enumerate(order)
+    return to_enum(:enumerate, order) unless block_given?
+
+    rivals.order(order).each do |group, score|
+      short_id = ShortId.new(group["short_id"])
+      character = Character.new(group["character"])
+      control_type = ControlType.new(group["control_type"])
+      name = group["name"]
+      yield({ short_id:, character:, control_type:, name:, score: })
+    end
+  end
+
+  def rivals
+    matchup
+      .performance
+      .group(away: [ :short_id, :character, :control_type ])
+      .select(
+        Arel.sql("ANY_VALUE(away.name)").as("name"),
+        away: [ :short_id, :character, :control_type ]
+      )
+      .limit(limit)
+  end
+end
