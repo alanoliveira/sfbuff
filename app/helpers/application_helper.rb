42d0ef3702f090960a8f15599b41cb485fe166a2
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-08 13:43:28 +0900

    add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def page_title(title)
     content_for(:title) { [ title, "SFBUFF" ].join(" - ") }
   end
+
+  def bs_icon(icon, **opts)
+    content_tag(:i, nil, class: [ "bi", "bi-#{icon}", opts.delete(:class) ], **opts)
+  end
 end
