2836bb6eb571a02032d85b8c9b52bba470ab8d5b
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-11 02:45:39 +0900

    localize websocket requests

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include Internationalizable
+
+    around_command :switch_locale, :switch_timezone
+
+    identified_by :session_id
+
+    def connect
+      reject_unauthorized_connection if request.session.id.blank?
+      self.session_id = request.session.id
+    end
   end
 end
