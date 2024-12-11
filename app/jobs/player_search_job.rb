4034569de4b0b375ef21ecec68572c77208b62df
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-11 23:57:11 +0900

    cache player search results

--- app/jobs/player_search_job.rb
+++ app/jobs/player_search_job.rb
@@ -4,7 +4,7 @@ class PlayerSearchJob < ApplicationJob
   queue_as :default
 
   def perform(query)
-    fighter_banner_list = FighterBanner.search(query)
+    fighter_banner_list = Rails.cache.fetch([ "FighterBanner#search", query ]) { FighterBanner.search(query) }
 
     cache_result partial: "fighter_banners/fighter_banner_list", locals: { fighter_banner_list: }
   end
