d4258dea25140bd5b5bb6ac8956e73fe9112982a
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-13 15:40:18 +0900

    change unsupported browsers handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   include Internationalizable
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   around_action :switch_locale, :switch_timezone
+  allow_browser versions: :modern, block: :handle_outdated_browser
+
+  private
+
+  def handle_outdated_browser
+    logger.info("Unsupported browser detected: #{request.user_agent}")
+    flash.now[:danger] = t("alerts.browser_not_supported").html_safe
+  end
 end
