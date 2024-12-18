65cf40fa32951c1c9cb558d4690fe87694e470ba
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-19 01:44:34 +0900

    normalize player search query parameter

--- app/jobs/player_search_job.rb
+++ app/jobs/player_search_job.rb
@@ -4,6 +4,7 @@ class PlayerSearchJob < ApplicationJob
   queue_as :default
 
   def perform(query)
+    query = query.strip.downcase
     fighter_banner_list = Rails.cache.fetch([ "FighterBanner#search", query ], expires_in: 10.minutes) { FighterBanner.search(query) }
 
     cache_result partial: "fighter_banners/fighter_banner_list", locals: { fighter_banner_list: }
--- spec/jobs/player_search_job_spec.rb
+++ spec/jobs/player_search_job_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe PlayerSearchJob, type: :job do
-  subject(:job) { described_class.perform_later(term) }
+  subject(:job) { described_class.perform_later(query) }
 
   around(:example) do |ex|
     Rails.with(cache: ActiveSupport::Cache.lookup_store(:memory_store)) do
@@ -9,12 +9,23 @@ RSpec.describe PlayerSearchJob, type: :job do
     end
   end
 
-  let(:term) { 'player 1234' }
+  let(:query) { "player 1234" }
   let(:response) { [] }
 
   before do
-    allow(FighterBanner).to receive(:search).with(term).and_return(response)
+    allow(FighterBanner).to receive(:search).with(query).and_return(response)
   end
 
   it_behaves_like "a streamable job"
+
+  context "when the query parameter is not normalized" do
+    let(:query) { "  NoT NoRmAlIZed  " }
+
+    it "normalizes the query before search" do
+      allow(FighterBanner).to receive(:search)
+
+      job.perform_now
+      expect(FighterBanner).to have_received(:search).with("not normalized")
+    end
+  end
 end
