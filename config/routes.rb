4f7251fd86cf77e524f1c3d9251c6f652cf58d8c
Alan Oliveira <alan.oliveira89@gmail.com>
2024-12-22 18:14:54 +0900

    fix pwa manifest route json restriction

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest.json" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest.json" => "rails/pwa#manifest", as: :pwa_manifest, format: false
 
   get "about" => "homepage#about", as: :about
 
