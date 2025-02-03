b1967ed503e95b09b8cc8f6dab3e861df638eabb
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-03 21:21:30 +0900

    add view chartjs component

---
+++ app/helpers/charts_helper.rb
@@ -0,0 +1,18 @@
+module ChartsHelper
+  CHART_COLORS = {
+    red: "rgb(255, 99, 132)",
+    orange: "rgb(255, 159, 64)",
+    yellow: "rgb(255, 205, 86)",
+    green: "rgb(75, 192, 192)",
+    blue: "rgb(54, 162, 235)",
+    purple: "rgb(153, 102, 255)",
+    grey: "rgb(201, 203, 207)"
+  }
+
+  def render_chart(chart_data)
+    tag.div data: { controller: "chartjs", chartjs_data_value: chart_data } do
+      concat tag.button icon("download"), class: "btn", data: { chartjs_target: "downloadButton" }
+      concat tag.canvas data: { chartjs_target: "canvas" }
+    end
+  end
+end
--- app/javascript/chartjs-plugins/visit.js
+++ app/javascript/chartjs-plugins/visit.js
@@ -2,13 +2,17 @@ export default {
   id: "visit",
   afterDraw: chart => {
     const config = chart.config.options.plugins.visit
-    if(!config) { return }
+    if (!config) { return }
 
     chart.options.onClick = evt => {
       const elements = chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true })
-      const target = elements[0] ? config.targets[elements[0].index] : null
-      if (target) {
-        const { url, options } = target
+      const firstPoint = elements[0]
+      if (!firstPoint) return
+
+      const item = chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
+      const visitTarget = item[config["property"]]
+      if (visitTarget) {
+        const { url, options } = visitTarget
         Turbo.visit(url, options)
       }
     }
