8088774c1bc05d51c404b1ea8aa5e35a9817b1a7
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-05 15:37:56 +0900

    add view helpers alert and icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alert(content = nil, type: "success", dismissible: true, **opts, &)
+    content ||= capture(&)
+    tag.div role: "alert", class: [ "alert", "alert-#{type}", ("alert-dismissible" if dismissible) ] do
+      concat content
+      concat button_tag("", type: "button", class: "btn-close", data: { bs_dismiss: "alert" }) if dismissible
+    end
+  end
+
+  def icon(icon, **opts)
+    tag.i nil, class: [ "bi", "bi-#{icon}", opts.delete(:class) ], **opts
+  end
 end
