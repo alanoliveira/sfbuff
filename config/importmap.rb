a270e7795a3287ab11e4662851addbe63d46d73b
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-06 16:01:33 +0900

    add view theme switcher

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 pin "chartjs", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"
 
 pin_all_from "app/javascript/helpers", under: "helpers"
