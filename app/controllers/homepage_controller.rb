d2e898c42f60dc13cfbb2dc4b2cce1266a26c504
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-06 20:52:28 +0900

    add about view

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,4 +3,7 @@ class HomepageController < ApplicationController
 
   def index
   end
+
+  def about
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def page_title(title = nil)
+    content_for :title, [ title, "SFBUFF" ].join(" - ")
+  end
+
   def alert(content = nil, type: "success", dismissible: true, **opts, &)
     content ||= capture(&)
     tag.div role: "alert", class: [ "alert", "alert-#{type}", ("alert-dismissible" if dismissible) ] do
