a13f1c06988710fdcf8f4c64303a01106ab5204a
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-10 11:39:39 +0900

    fix battle/show finding method

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -2,6 +2,6 @@ class BattlesController < ApplicationController
   before_action { fresh_when etag: params[:replay_id] }
 
   def show
-    @battle = Battle.find_by(replay_id: params[:replay_id])
+    @battle = Battle.find_by!(replay_id: params[:replay_id])
   end
 end
