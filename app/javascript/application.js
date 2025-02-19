2c12a347fe2aa6497de5544afa1500955093dce4
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-20 01:53:04 +0900

    fix bootstrap importing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
 
 import { setCookie, getCookie } from "helpers/cookie_helpers"
 if(!getCookie("timezone")) {
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import "bootstrap"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
