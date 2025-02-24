08c59612b8db36724c150febc928ee62b286838b
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-24 20:07:22 +0900

    Revert "use pagy_countless if search range is gt 1.month"
    This reverts commit f46b6c0de44c2d63744778c4518f76f236557411.

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,6 +1,4 @@
-require "pagy/countless"
 require "pagy/extras/bootstrap"
 require "pagy/extras/overflow"
-require "pagy/extras/countless"
 Pagy::I18n.load(*I18n.available_locales.map { { locale: _1.to_s } })
-Pagy::DEFAULT[:overflow] = :empty_page
+Pagy::DEFAULT[:overflow] = :last_page
