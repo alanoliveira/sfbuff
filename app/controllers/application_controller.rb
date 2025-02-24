65be820458010e5cc98b9875d5e86ec989eef854
Alan Oliveira <alan.oliveira89@gmail.com>
2025-02-24 20:12:24 +0900

    track slow actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   before_action :require_timezone
   around_action :switch_timezone, :switch_locale
+  around_action :track_slow_requests
 
   etag { I18n.locale.name }
   etag { Rails.application.config.git_revision }
@@ -15,5 +16,15 @@ class ApplicationController < ActionController::Base
 
   def too_many_requests
     render partial: "application/too_many_requests", status: :too_many_requests
+  end
+
+  def track_slow_requests
+    start_time = Time.now
+    yield
+    end_time = Time.now
+    diff = end_time - start_time
+    if diff > 1.second
+      ahoy.track("slow_request", params: params.to_unsafe_h, time: diff)
+    end
   end
 end
