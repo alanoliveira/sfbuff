ad73497e458a9efb9a75dbd56a7756cf050b3653
Alan Oliveira <alan.oliveira89@gmail.com>
2025-08-07 15:32:41 +0900

    refactor ranked_history to use RankedStep

--- app/models/ranked_history.rb
+++ app/models/ranked_history.rb
@@ -1,45 +1,23 @@
 class RankedHistory
+  include Enumerable
+  extend ActiveModel::Translation
+
   Step = Data.define(:played_at, :replay_id, :mr, :lp, :mr_variation, :lp_variation) do
     def calibrating?
       mr == 0 && lp == -1
     end
   end
 
-  include Enumerable
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-
-  QUERY = <<-SQL
-    SELECT * FROM (
-      SELECT replay_id,
-             played_at,
-             p1_fighter_id fighter_id,
-             p1_character_id character_id,
-             p1_mr mr,
-             p1_lp lp
-      FROM battles
-      WHERE battle_type_id = 1
-      UNION ALL
-      SELECT replay_id,
-             played_at,
-             p2_fighter_id fighter_id,
-             p2_character_id character_id,
-             p2_mr mr,
-             p2_lp lp
-      FROM battles
-      WHERE battle_type_id = 1
-    ) WHERE fighter_id = :fighter_id
-        AND character_id = :character_id
-        AND played_at BETWEEN :from_date AND :to_date
-      ORDER BY played_at
-  SQL
-
-  attribute :fighter_id
-  attribute :character_id
-  attribute :from_date, default: Date.today
-  attribute :to_date, default: Date.today
+  attr_reader :fighter_id, :character_id, :from_date, :to_date
 
   delegate :any?, to: :execute
+
+  def initialize(fighter_id:, character_id:, from_date: nil, to_date: nil)
+    @fighter_id = fighter_id
+    @character_id = character_id
+    @from_date = from_date || Date.today
+    @to_date = to_date || Date.today
+  end
 
   def each(&)
     data.each(&)
@@ -48,7 +26,7 @@ class RankedHistory
   private
 
   def data
-    execute.chain([ nil ]).each_cons(2).map do |data1, data2|
+    @data ||= execute.chain([ nil ]).each_cons(2).map do |data1, data2|
       Step.new(
         played_at: data1["played_at"],
         replay_id: data1["replay_id"],
@@ -61,21 +39,13 @@ class RankedHistory
   end
 
   def execute
+    played_between = from_date.beginning_of_day .. to_date.end_of_day
     ActiveRecord::Base
       .lease_connection
-      .select_all(sanitized_query)
+      .select_all(ranked_steps.where(played_at: played_between))
   end
 
-  def sanitized_query
-    ApplicationRecord.sanitize_sql([ QUERY, query_params ])
-  end
-
-  def query_params
-    {
-      fighter_id:,
-      character_id:,
-      from_date: from_date&.beginning_of_day,
-      to_date: to_date&.end_of_day
-    }
+  def ranked_steps
+    RankedStep.where(fighter_id:, character_id:).order(:played_at)
   end
 end
