0c6e6a25a0d9e1b5f0e3726c6d4da569a62548bc
Alan Oliveira <alan.oliveira89@gmail.com>
2025-08-10 22:54:24 +0900

    increase atempts and time between retrying a job after BadGateway error

--- app/jobs/concerns/bad_gateway_handleable.rb
+++ app/jobs/concerns/bad_gateway_handleable.rb
@@ -1,5 +1,5 @@
 module BadGatewayHandleable
   extend ActiveSupport::Concern
 
-  included { retry_on BucklerApi::Errors::BadGateway, attempts: 2 }
+  included { retry_on BucklerApi::Errors::BadGateway, attempts: 3, wait: 6.seconds }
 end
