5ef656858c7bd381d897cd896e6a8f8ec1912f1c
Alan Oliveira <alan.oliveira89@gmail.com>
2025-11-30 14:10:18 +0900

    implement rivals model

---
+++ app/models/rivals.rb
@@ -0,0 +1,39 @@
+class Rivals
+  Result = Struct.new(:name, :fighter_id, :character_id, :input_type_id, :score) do
+    def character = Character[character_id]
+    def input_type = InputType[input_type_id]
+  end
+
+  def initialize(matches)
+    @matches = matches
+      .group("away_fighter_id", "away_character_id", "away_input_type_id")
+      .select(
+        "away_fighter_id fighter_id",
+        "away_character_id character_id",
+        "away_input_type_id input_type_id",
+        "ANY_VALUE(away_name) name"
+      )
+  end
+
+  def favorites(limit = nil)
+    fetch_data(limit, { "total" => "desc", "wins" => "desc", "losses" => "desc" })
+  end
+
+  def victims(limit = nil)
+    fetch_data(limit, { "diff" => "desc", "losses" => "asc", "wins" => "desc" })
+  end
+
+  def tormentors(limit = nil)
+    fetch_data(limit, { "diff" => "asc", "wins" => "asc", "losses" => "desc" })
+  end
+
+  private
+
+  def fetch_data(limit, order)
+    @matches
+      .limit(limit)
+      .order(order)
+      .aggregate_results
+      .map { |data, score| Result.new(**data, score:) }
+  end
+end
