8c2cf78ce6b6d3cdf7be4bff0839502bf5f5c863
Alan Oliveira <alan.oliveira89@gmail.com>
2025-11-30 21:39:03 +0900

    limit concurrency for request jobs

--- app/jobs/search_request/process_job.rb
+++ app/jobs/search_request/process_job.rb
@@ -1,4 +1,5 @@
 class SearchRequest::ProcessJob < ApplicationJob
+  limits_concurrency to: 1, key: ->(search_request) { search_request.query }, duration: 1.minute
   queue_as :default
 
   def perform(search_request)
--- app/jobs/synchronization_request/process_job.rb
+++ app/jobs/synchronization_request/process_job.rb
@@ -1,4 +1,5 @@
 class SynchronizationRequest::ProcessJob < ApplicationJob
+  limits_concurrency to: 1, key: ->(synchronization_request) { synchronization_request.fighter_id }, duration: 1.minute
   queue_as :default
 
   def perform(synchronization_request)
