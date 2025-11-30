8c109db8cc416f5bb16074a72abc796eb755092f
Alan Oliveira <alan.oliveira89@gmail.com>
2025-11-30 21:42:20 +0900

    add es-module-shims

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
   include SwitchLocale
   include SwitchTimezone
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
