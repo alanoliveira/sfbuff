a79d8949b079aa65bfe631e9df502a4d630c5db7
Alan Oliveira <alan.oliveira89@gmail.com>
2025-11-30 18:26:44 +0900

    local-time i18n

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import LocalTime from "local-time"
+import { ptBR, ja } from "local-time-i18n"
 
+LocalTime.config.i18n["pt-BR"] = ptBR
+LocalTime.config.i18n["ja"] = ja
+LocalTime.config.locale = document.documentElement.lang
 LocalTime.start()
+
 document.addEventListener("turbo:morph", () => {
   LocalTime.run()
 })
