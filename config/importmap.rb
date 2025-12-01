3f3b57a30d3543022b336135eba8e7502a2e31ef
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-01 19:55:18 +0900

    use jsdelivr assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,9 +9,18 @@ pin_all_from "app/javascript/chartjs/plugins", under: "chartjs-plugins", to: "ch
 pin_all_from "app/javascript/helpers", under: "helpers"
 pin_all_from "app/javascript/local-time-i18n", under: "local-time-i18n"
 
-pin "bootstrap", to: "/assets/bootstrap/dist/js/bootstrap.bundle.min.js"
-pin "local-time", to: "/assets/local-time/app/assets/javascripts/local-time.es2017-esm.js"
-pin "chartjs", to: "/assets/chart.js/dist/chart.umd.js"
-pin "chartjs-adapter-date-fns", to: "/assets/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"
-pin "date-fns/locale/pt-BR", to: "/assets/date-fns/locale/pt-BR.js"
-pin "date-fns/locale/ja", to: "/assets/date-fns/locale/ja.js"
+if ENV["SFBUFF_USE_LOCAL_ASSETS"]
+  pin "bootstrap", to: "/assets/bootstrap/dist/js/bootstrap.bundle.min.js"
+  pin "local-time", to: "/assets/local-time/app/assets/javascripts/local-time.es2017-esm.js"
+  pin "chartjs", to: "/assets/chart.js/dist/chart.umd.js"
+  pin "chartjs-adapter-date-fns", to: "/assets/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"
+  pin "date-fns/locale/pt-BR", to: "/assets/date-fns/locale/pt-BR.js"
+  pin "date-fns/locale/ja", to: "/assets/date-fns/locale/ja.js"
+else
+  pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+  pin "local-time", to: "https://cdn.jsdelivr.net/npm/local-time@3.0.3/app/assets/javascripts/local-time.es2017-esm.js"
+  pin "chartjs", to: "https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.umd.js"
+  pin "chartjs-adapter-date-fns", to: "https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js"
+  pin "date-fns/locale/pt-BR", to: "https://cdn.jsdelivr.net/npm/date-fns@4.1.0/locale/pt-BR.js"
+  pin "date-fns/locale/ja", to: "https://cdn.jsdelivr.net/npm/date-fns@4.1.0/locale/ja.js"
+end
