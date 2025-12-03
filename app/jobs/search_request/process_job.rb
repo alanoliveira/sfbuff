42e7cc19b535f4f37b3e434b8122c74799f46a77
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-03 15:07:06 +0900

    rescue from buckler api http errors on process jobs

--- app/jobs/search_request/process_job.rb
+++ app/jobs/search_request/process_job.rb
@@ -1,4 +1,6 @@
 class SearchRequest::ProcessJob < ApplicationJob
+  include RescueFromBucklerApiHttpErrors
+
   limits_concurrency to: 1, key: ->(search_request) { search_request.query }, duration: 1.minute
   queue_as :default
 
--- app/jobs/synchronization_request/process_job.rb
+++ app/jobs/synchronization_request/process_job.rb
@@ -1,4 +1,6 @@
 class SynchronizationRequest::ProcessJob < ApplicationJob
+  include RescueFromBucklerApiHttpErrors
+
   limits_concurrency to: 1, key: ->(synchronization_request) { synchronization_request.fighter_id }, duration: 1.minute
   queue_as :default
 
