61b5cbb77aa8271d3f4aeaa7bfb091d8dca18da1
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-03 21:09:31 +0900

    fix buckler_site_client test

--- spec/clients/buckler_site_client_spec.rb
+++ spec/clients/buckler_site_client_spec.rb
@@ -9,26 +9,30 @@ RSpec.describe BucklerSiteClient do
       .with(headers: { "User-Agent" => described_class.user_agent })
   end
 
+  before do
+    described_class.user_agent = "test-agent"
+  end
+
   describe "#next_data" do
     let(:response) { <<-HTML }
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title></title>
-          <link href="css/style.css" rel="stylesheet">
-        </head>
-        <body>
-          <script id="__NEXT_DATA__" type="application/json">
-          {"response": "ok"}
-          </script>
-        </body>
-      </html>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <link href="css/style.css" rel="stylesheet">
+      </head>
+      <body>
+        <script id="__NEXT_DATA__" type="application/json">
+        {"response": "ok"}
+        </script>
+      </body>
+    </html>
     HTML
 
     before do
-        stub_request(:get, "/6/buckler").to_return(body: response)
+      stub_request(:get, "/6/buckler").to_return(body: response)
     end
 
     it { expect(buckler_site_client.next_data).to eq({ "response" => "ok" }) }
