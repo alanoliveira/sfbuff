9f0c97aba5299d2be4b3cd3c413234da695704ca
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-04 19:37:29 +0900

    remove away parameters when filtering matches for matchup chart

--- app/controllers/fighters/matchup_charts_controller.rb
+++ app/controllers/fighters/matchup_charts_controller.rb
@@ -7,4 +7,10 @@ class Fighters::MatchupChartsController < ApplicationController
   def show
     @matchup_chart = MatchupChart.new(@matches_filter.filter(@fighter.matches))
   end
+
+  private
+
+  def matches_filter_params
+    super.without(:away_fighter_id, :away_character_id, :away_input_type_id)
+  end
 end
