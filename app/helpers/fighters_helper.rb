f07182207bacdbfc5b335ec76f58e099be733e95
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-05 17:02:53 +0900

    make fighter links target _top

--- app/helpers/fighters_helper.rb
+++ app/helpers/fighters_helper.rb
@@ -1,4 +1,9 @@
 module FightersHelper
+  def link_to_fighter(name, fighter, data: {}, **)
+    data[:turbo_frame] ||= "_top"
+    link_to(name, fighter_path(fighter), data:, **)
+  end
+
   def fighter_header_nav_item(name, route_name, params: {})
     content_tag :li, class: "nav-item" do
       link_to name, url_for([ route_name, params ]), class: [ "nav-link", ("active" if current_page? route_name) ]
