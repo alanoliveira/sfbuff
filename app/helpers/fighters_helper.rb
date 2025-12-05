3da3fdbb3331cbf6ec5405a17976e7530ae8db2f
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-05 17:40:03 +0900

    add a link to official site profile on fighter header

--- app/helpers/fighters_helper.rb
+++ app/helpers/fighters_helper.rb
@@ -9,4 +9,8 @@ module FightersHelper
       link_to name, url_for([ route_name, params ]), class: [ "nav-link", ("active" if current_page? route_name) ]
     end
   end
+
+  def link_to_fighter_buckler_profile(fighter, **, &)
+    link_to("https://www.streetfighter.com/6/buckler/profile/#{fighter.id}", target: "_blank", rel: "nofollow", **, &)
+  end
 end
