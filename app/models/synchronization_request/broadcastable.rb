d5e90f34e20450bb0236ca35c48c9891bbf4e910
Alan Oliveira <alan.oliveira89@gmail.com>
2025-12-24 18:15:58 +0900

    remove synchronization_request code

--- app/models/synchronization_request/broadcastable.rb
+++
@@ -1,9 +0,0 @@
-module SynchronizationRequest::Broadcastable
-  extend ActiveSupport::Concern
-  include WorkerBroadcaster
-
-  included do
-    after_subscribe :process_later, if: :created?
-    after_save_commit :broadcast_replace_later, if: :finished?
-  end
-end
--- spec/factories/synchronization_requests.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :synchronization_request do
-    fighter_id { "" }
-    status { 1 }
-    result { "" }
-    error { "MyString" }
-  end
-end
