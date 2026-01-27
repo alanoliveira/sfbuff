18d4bba0cb0f329a0e812f5c8a4914fc1eee8e4f
Alan Oliveira <alan.oliveira89@gmail.com>
2026-01-27 12:12:12 +0900

    change settings for fighter synchronization queue

--- app/jobs/fighter/synchronization/process_job.rb
+++ app/jobs/fighter/synchronization/process_job.rb
@@ -3,7 +3,7 @@ class Fighter::Synchronization::ProcessJob < ApplicationJob
 
   limits_concurrency to: 1, key: ->(fighter_synchronization) { fighter_synchronization.fighter_id }, duration: 1.minute
 
-  queue_as :default
+  queue_as :synchronization
 
   def perform(fighter_synchronization)
     fighter_synchronization.process
