a292ea3745a1dca114dae1efa334ed5a7939ab79
Alan Oliveira <alan.oliveira89@gmail.com>
2026-01-31 00:38:45 +0900

    fetch all new replays before start to save and add logging on battle synchronizer

--- app/models/fighter/synchronization/battles_synchronizer.rb
+++ app/models/fighter/synchronization/battles_synchronizer.rb
@@ -8,12 +8,22 @@ class Fighter::Synchronization::BattlesSynchronizer
   def synchronize
     @synchronized_battles = BucklerApiGateway
       .fetch_fighter_replays(fighter.id)
-      .map { |replay| Battle.create_or_find_by(replay_id: replay.replay_id) { it.from_replay(replay) } }
-      .take_while { it.replay_id != fighter_last_synchronized_replay_id }
+      .take_while { |replay| replay.replay_id != fighter_last_synchronized_replay_id }
+      .map { |replay| create_or_find_battle_by_replay(replay)  }
       .entries
   end
 
   private
+
+  def create_or_find_battle_by_replay(replay)
+    Battle.create_or_find_by(replay_id: replay.replay_id) { it.from_replay(replay) }.tap do |battle|
+      if battle.previously_new_record?
+        Rails.logger.info("[#{self.class.name}] Battle #{battle.replay_id} just imported")
+      else
+        Rails.logger.info("[#{self.class.name}] Battle #{battle.replay_id} already imported")
+      end
+    end
+  end
 
   def fighter_last_synchronized_replay_id
     @fighter_last_synchronized_replay_id ||= Fighter::Synchronization
