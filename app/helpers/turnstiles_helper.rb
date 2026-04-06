1bb170dc70dceb4c8c03bcb7f26b149aa51e2d81
Alan Oliveira <alan.oliveira89@gmail.com>
2026-04-06 20:20:08 +0900

    prevent turnstile caching

--- app/helpers/turnstiles_helper.rb
+++ app/helpers/turnstiles_helper.rb
@@ -1,6 +1,6 @@
 module TurnstilesHelper
   def turnstile_form
-    form_with url: human_verification_path, data: { controller: "turnstile-form", turnstile_form_sitekey_value: HumanVerification.cf_turnstile_site_key } do |f|
+    form_with url: human_verification_path, data: { turbo_temporary: true, controller: "turnstile-form", turnstile_form_sitekey_value: HumanVerification.cf_turnstile_site_key } do |f|
       content_tag :div, nil, data: { turnstile_form_target: "widget" }
     end
   end
